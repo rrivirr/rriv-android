db93306ceabf2b623faf206d835feb1318c7638d
Taofeek’s MacBook Pro <oduola.taofeekkola@gmail.com>
2023-03-19 02:27:46 +0100

    Implemented mik3y:usb-serial-for-android Successfully detected attached usb accesories as it was unable to with the felhr85 library Moved to MavenCentral

--- app/src/main/java/org/rriv/android/MainActivity.kt
+++ app/src/main/java/org/rriv/android/MainActivity.kt
@@ -1,8 +1,10 @@
 package org.rriv.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import org.rriv.android.results.OutputScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         val actionBar: ActionBar? = supportActionBar
         if (actionBar != null) actionBar.hide()
         setContentView(R.layout.activity_main)
+    }
+    override fun onNewIntent(intent: Intent) {
+        if ("android.hardware.usb.action.USB_DEVICE_ATTACHED" == intent.action) {
+            val terminal: OutputScreenFragment? =
+                supportFragmentManager.findFragmentByTag("terminal") as OutputScreenFragment?
+            if (terminal != null) terminal.status("USB device detected")
+        }
+        super.onNewIntent(intent)
     }
 }
